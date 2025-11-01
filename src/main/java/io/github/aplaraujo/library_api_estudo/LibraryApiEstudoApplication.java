fbaf18987e100f1500cc3be5565e91335dba6bd6
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-31 22:03:05 -0300

    first commit

---
+++ src/main/java/io/github/aplaraujo/library_api_estudo/LibraryApiEstudoApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo.library_api_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApiEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aplaraujo/library_api_estudo/LibraryApiEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo.library_api_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
