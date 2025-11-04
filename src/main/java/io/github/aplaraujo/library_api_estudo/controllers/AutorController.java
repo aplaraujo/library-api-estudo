3f459a50dde41103479e59e73a2670cf731e085f
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-04 17:19:27 -0300

    Refatoração com Lombok

--- src/main/java/io/github/aplaraujo/library_api_estudo/controllers/AutorController.java
+++ src/main/java/io/github/aplaraujo/library_api_estudo/controllers/AutorController.java
@@ -6,6 +6,7 @@ import io.github.aplaraujo.library_api_estudo.exceptions.OperacaoNaoPermitidaExc
 import io.github.aplaraujo.library_api_estudo.exceptions.RegistroDuplicadoException;
 import io.github.aplaraujo.library_api_estudo.model.Autor;
 import io.github.aplaraujo.library_api_estudo.services.AutorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +20,11 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/autores") // http://localhost:8080/autores
-
+@RequiredArgsConstructor
 public class AutorController { // Camada de entrada de dados do sistema
 
     private final AutorService autorService;
-
-    public AutorController(AutorService autorService) {
-        this.autorService = autorService;
-    }
-
+    
     // Formas de criar uma requisição
     // Response Entity - classe que representa uma resposta a uma requisição
 //    @RequestMapping(method = RequestMethod.POST)
