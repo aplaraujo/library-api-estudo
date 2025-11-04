831db8820e4418b3664c0a800550c205c81b85ef
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-04 20:23:37 -0300

    Validações de cadastro de livros

--- src/main/java/io/github/aplaraujo/library_api_estudo/controllers/LivroController.java
+++ src/main/java/io/github/aplaraujo/library_api_estudo/controllers/LivroController.java
@@ -1,7 +1,14 @@
 package io.github.aplaraujo.library_api_estudo.controllers;
 
+import io.github.aplaraujo.library_api_estudo.controllers.dto.CadastroLivroDTO;
+import io.github.aplaraujo.library_api_estudo.controllers.dto.ErroResposta;
+import io.github.aplaraujo.library_api_estudo.exceptions.RegistroDuplicadoException;
 import io.github.aplaraujo.library_api_estudo.services.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LivroController {
     private final LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
+        try {
+            // Mapear dto para a entidade
+            // Enviar a entidade para o serviço, validação e gravação na base
+            // Criar url de acesso dos dados do livro
+            // Retornar código criado com cabeçalho
+            return ResponseEntity.ok(dto);
+        } catch (RegistroDuplicadoException e) {
+            var erro = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erro.status()).body(erro);
+        }
+    }
 }
--- src/main/java/io/github/aplaraujo/library_api_estudo/controllers/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/aplaraujo/library_api_estudo/controllers/dto/CadastroLivroDTO.java
@@ -1,17 +1,33 @@
 package io.github.aplaraujo.library_api_estudo.controllers.dto;
 
 import io.github.aplaraujo.library_api_estudo.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "campo obrigatório")
         String isbn,
+
+        @NotBlank(message = "campo obrigatório")
+        @Size(max = 150, message = "Campo fora do tamanho padrão")
         String titulo,
+
+        @NotNull(message = "campo obrigatório")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataPublicacao,
+
         GeneroLivro genero,
         BigDecimal preco,
+
+        @NotNull(message = "campo obrigatório")
         UUID idAutor
 ) {
 }
